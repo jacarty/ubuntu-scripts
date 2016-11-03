51782ba1f7ecc3ac41d8d08ff422d7e1f920085c
James Carty <jacarty@users.noreply.github.com>
2016-11-03 13:19:27 +0000

    Add Sudo prompt to beginning of script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 apt-get remove -y libreoffice-* openoffice* unity-webapps-common
 apt autoremove -y
 apt-get update && apt-get dist-upgrade -y
