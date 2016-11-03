24925e62262c4aa63a5fbf53240e28cb14292189
James Carty <jacarty@users.noreply.github.com>
2016-11-03 10:49:53 +0000

    Add $user var for Docker group and change package commands

--- setup-with-docker.sh
+++ setup-with-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 apt-get remove -y libreoffice-* openoffice* unity-webapps-common
-apt-get update
-apt-get upgrade -y
-apt-get install -y open-vm-tools open-vm-tools-desktop vim
+apt autoremove -y
+apt-get update && apt-get dist-upgrade -y
+apt-get install -y open-vm-tools open-vm-tools-desktop openssh-server vim
 wget -qO- https://get.docker.com | sh
-sudo usermod -aG docker us
+usermod -aG docker $USER
 reboot
