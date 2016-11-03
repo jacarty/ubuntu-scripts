d833ff6d7d58d037297d19cc38c81ccf6f73aba6
James Carty <jacarty@users.noreply.github.com>
2016-11-03 15:11:54 +0000

    Remove additional packages from install

--- setup-with-docker.sh
+++ setup-with-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
-apt-get remove -y libreoffice-* openoffice* unity-webapps-common
+apt-get remove -y libreoffice-* openoffice* unity-webapps-common gnome-calendar gnome-sudoku gnome-mahjongg gnome-mines cheese* rhythmbox* shotwell* thunderbird* aisleriot
 apt autoremove -y
 apt-get update && apt-get dist-upgrade -y
 apt-get install -y open-vm-tools open-vm-tools-desktop openssh-server vim
