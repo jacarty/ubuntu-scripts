378c2a2006759f5a06777c39f2ece534ed0f750e
James Carty <jacarty@cisco.com>
2017-07-02 18:28:20 +0100

    Modify to configure timezone

--- setup.sh
+++ setup.sh
@@ -7,10 +7,11 @@
 echo "Would you like to install docker too? Please respond Y or N."
 read docker
 
-#Remove junk, upgrade and install VMTools, SSH Server and VIM
+#Remove junk, upgrade, set timezone, install VMTools, SSH Server and VIM
 apt-get remove -y libreoffice-* openoffice* unity-webapps-common gnome-calendar gnome-sudoku gnome-mahjongg gnome-mines cheese* rhythmbox* shotwell* thunderbird* aisleriot
 apt autoremove -y
 apt-get update && apt-get dist-upgrade -y
+timedatectl set-timezone Europe/London
 apt-get install -y open-vm-tools open-vm-tools-desktop openssh-server vim
 
 #Install Docker if needed
